be6a20fa33e949f369ca7b44a69f0d01efcfebce
Rafael Gomez Guillen <gomezra72@outlook.com>
2023-05-31 16:21:42 +0100

    Uploading proyect

---
+++ FormulaOne/Client/Program.cs
@@ -0,0 +1,28 @@
+using Blazorise;
+using Blazorise.Icons.Material;
+using Blazorise.Material;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace FormulaOne
+{
+    public class Program
+    {
+        public static async Task Main (string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services
+            .AddBlazorise(options =>
+            {
+                options.Immediate = true;
+            })
+            .AddMaterialProviders()
+            .AddMaterialIcons();
+            await builder.Build().RunAsync();
+        }
+    }
+}
